d1678d4cc729748bef63c96e0621be998a2bcf10
fieldr <fieldr92@gmail.com>
2019-02-06 14:59:53 +0000

    Action creators for CRUD - POST working to json-server - thunk set up

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends Component {
   renderError({ error, touched }) {
@@ -23,10 +25,9 @@ class StreamCreate extends Component {
     );
   };
 
-  onSubmit(formValues) {
-    // NO EVENT OBJECT NEEDED BECAUSE OF redux-form
-    console.log(formValues);
-  }
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -53,7 +54,12 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate);
+
+export default connect(
+  null,
+  { createStream }
+)(formWrapped);
